1cd65aea4809729d3dbf8085612ca2361c1bb7df
Ivanzino <165265832+Ivanzino@users.noreply.github.com>
2025-10-21 15:46:42 -0500

    Add files via upload

---
+++ base.py
@@ -0,0 +1,9 @@
+# from flask import Blueprint, request, render_template, redirect, url_for, flash
+
+
+# base = Blueprint('base', __name__, template_folder='Src/template')
+
+
+# @base.route('/')
+# def index():
+#     return render_template('../HorariosSena/Src/template/indexfinal.html')
---
+++ main.py
@@ -0,0 +1,9 @@
+
+# from Src import app
+# from HorariosSena import base
+
+# app.register_blueprint(base)
+
+# # starting the app
+# if __name__ == "__main__":
+#     app.run(port=3000, debug=True)
